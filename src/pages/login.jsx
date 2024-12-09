a674a94775e51f0a7a92c19c34546e3765d61533
Mukilan K <134675440+mukilankumar@users.noreply.github.com>
2024-12-09 16:52:54 +0530

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
     setError('');
 
     try {
-      const response = await axios.post('http://localhost:8000/login', {
+      const response = await axios.post('https://chat-2-1dgm.onrender.com/login', {
         employeeId,
         password
       });
